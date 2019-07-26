4f3c0952df0da25937ffd3315e500352c8775392
Cameron Schilling <42946844+sgtschilling@users.noreply.github.com>
2019-07-26 11:54:45 -0500

    Update Index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+import dotenv from 'dotenv'
+dotenv.config();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
