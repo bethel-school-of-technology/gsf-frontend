db2ac6a0a4998767e347b8f6600829e390afb53a
Cameron Schilling <42946844+sgtschilling@users.noreply.github.com>
2019-07-26 13:44:27 -0500

    added content instead of placeholders

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -8,23 +8,28 @@ class About extends Component {
     return (
       <div>
         <div className="landing">
+          <div className="aboutBG">
           <div className="dark-overlay landing-inner text-light">
+            
             <div className="container">
               <div className="row">
                 <div className="col-md-12 text-center">
                   <div className="about-header">
-                  <h3 className="display-3 mb-4">Genesis Strengh and Fitness -- GSF</h3>
+                  <h1 className="display-3 mb-4">GSF</h1>
                   <p className = "lead">Here For Your Fitness Journey From The Very Beginning.</p>
-                  <p className="lead"> Track Workouts, See Upcoming Events, Stay Informed, Let's Progress Together.</p>
+                  <p className="lead">Let's Progress Together.</p>
                   </div>
                   <div className="grid-container">
                     
-                    <p>Placeholder‬</p>
+                    <h2>One thing about Genesis Strength and Fitness:
+                    </h2>
                       <br />
-                    <p>Placeholder</p>
+                    <h4 class name = "aboutWords">We may not have the "latest" and "greatest" equipment, and your hands will get dusty from chalk, but what we DO have is an atmosphere of faith and friendship.
+                      Not only faith in God, but faith that we will help each other. Faith that we will not let others fail. Faith that if they do fail, we will lift them up again.
+                    </h4>
                     <br />
                     <h5> 
-                      Placeholder<br/>
+                      Proverbs 27:17-"Iron sharpens iron, so one person sharpens another."<br/>
                       </h5>
 
                   </div>
@@ -33,10 +38,10 @@ class About extends Component {
             </div>
           </div>
         </div>
+        </div>
       </div>
     );
   }
 }
 
 export default About;
-
